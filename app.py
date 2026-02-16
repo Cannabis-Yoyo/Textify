f2789ab8e49fd81b54da65409b58f533877c0b6f
Cannabis-Yoyo <myworkproject215@gmail.com>
2026-02-16 15:53:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ for pkg in nltk_packages:
     except LookupError:
         nltk.download(pkg, download_dir=nltk_data_dir, quiet=True)
 
-import os
+# import os
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HF_TOKEN"]
 
 
@@ -282,6 +282,7 @@ if st.sidebar.button("🚀 Analyze Text"):
             st.markdown("<div class='card'>No actionable insights found</div>", unsafe_allow_html=True)
 
         st.success("Analysis completed successfully.")
+
 
 
 
